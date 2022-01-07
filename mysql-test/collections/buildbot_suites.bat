b7a814eac3efcfe8c602879c9555a779d373297e
Vladislav Vaintroub <wlad@mariadb.com>
2022-01-07 20:19:45 +0100

    Windows, CI  - workaround hardcoded limits for mtr --parallel=auto

--- mysql-test/collections/buildbot_suites.bat
+++ mysql-test/collections/buildbot_suites.bat
@@ -1,4 +1,5 @@
-perl mysql-test-run.pl --verbose-restart --force --suite-timeout=120 --max-test-fail=10 --retry=3  --parallel=auto --suite=^
+if "%MTR_PARALLEL%"=="" set /a MTR_PARALLEL=%NUMBER_OF_PROCESSORS%*3/2
+perl mysql-test-run.pl --verbose-restart --force --suite-timeout=120 --max-test-fail=10 --retry=3  --suite=^
 vcol,gcol,perfschema,^
 main,^
 innodb,^
